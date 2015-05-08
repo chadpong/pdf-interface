ee62acca41d9babe312b82f7ef7ca6dd9c942075
chadpong <p.chadpong@gmail.com>
2015-05-08 18:31:47 +0700

    transfer

--- pdf-interface.js
+++ pdf-interface.js
@@ -7,92 +7,87 @@
 	};
 
 	var docDefinition = {
-			pageSize: 'A4',
-			pageMargins: [20, 100, 20, 70], // left, top, right, bottom
-	 		header: {
-	 			margin: 5,
-	 			columns: [
-					{
-						image: 'nhso_logo',
-						width: 200
-					},
-					{
-						stack : [
-							{
-								margin : [20, 20, 20, 0],
-								text : 'โครงการพัฒนาฐานข้อมูลและสารสนเทศเพื่อการดูแลผู้สูงอายุระยะยาว',
-								fontSize : 18,
-								bold : true,
-								alignment: 'right',
-							},
-							{
-								margin : [20, 22, 20, 0],
-								text : 'รายงานการดูแลผู้สูงอายุระยะยาวสำหรับผู้จัดการรายกรณี',
-								fontSize : 16,
-								bold : true,
-								alignment: 'right',
-							}
-						]	
-					}				
-				]				       
+			pageMargins: [20, 90, 20, 70], // left, top, right, bottom
+	 		header: { 				
+				margin : [10, 0,0,0],
+				image: 'nhso_logo',
+				width: 200				       
 		    },
 		    footer: {		    	
-		        margin: [75 , 0, 0, 0],
+		        margin: [20 , 5, 0, 0],
 	 			columns: [
 					{
 						image: 'nu_logo',
-						width: 50
-					},
-					{
-						stack : [
-
-							{
-								margin : [20, 5, 20, 0],
-								text : 'จัดทำโดย มหาวิทยาลัยนเรศวร จังหวัดพิษณุโลก',
-								fontSize : 18,
-								bold : true,
-								alignment: 'left',
-							},
-							{
-								margin : [20, 5, 20, 0],
-								text : 'E-mail : childwelfare.nu@gmail.com Facebook : www.facebook.com/SeniorLTC',
-								fontSize : 16,
-								bold : true,
-								alignment: 'left',
-							}
-						]	
-					}				
+						width: 200,
+						alignment : 'center'
+					}								
 				]	
 		    },		    
-	 		content: [
-	 			{canvas: [{ type: 'line', x1: 0, y1: 5, x2: 595-2*20, y2: 5, lineWidth: 1 }]},
-	 			{
-	 				margin : [0, 20, 0, 0],
-	 				text : "ข้อตกลงแผนการดูแลผู้สูงอายุ",
-	 				fontSize : 16,
-	 				bold : true,
-	 				alignment : 'center'
-	 			},
-	 			{
-	 				text : '(Care Plan Agreement)',
-	 				fontSize : 16,
-	 				bold : true,
-	 				alignment : 'center'
-	 			}
-	 					    
+	 		content: [	 			
+ 				{
+ 					 text: 'ข้อตกลงการดูแล',
+ 					 fontSize : 16,
+ 					 alignment : 'center'
+ 				},	 				
+ 				{
+	 				table: {
+						widths: [100, '*', 200, '*'],
+						headerRows: 1,
+						body: [
+							[ 't_header', 't_header', 't_header', 't_header'],
+							[ 'aaa', 'bbb', 'ccc', 'dddd'],
+							[ 'aaa', 'bbb', 'ccc', 'dddd\naaaa'],
+							[ 'aaa', 'bbb', 'ccc', 'dddd'],
+							[ 'aaa', 'bbb', 'ccc', 'dddd'],
+							[ 'aaa', 'bbb', 'ccc', 'dddd'],
+							[ 'aaa', 'bbb', 'ccc', 'dddd'],
+							[ 'aaa', 'bbb', 'ccc', 'dddd'],
+							[ 'aaa', 'bbb', 'ccc', 'dddd'],
+							[ 'aaa', 'bbb', 'ccc', 'dddd'],
+							[ 'aaa', 'bbb', 'ccc', 'dddd'],
+							[ 'aaa', 'bbb', 'ccc', 'dddd'],
+							[ 'aaa', 'bbb', 'ccc', 'dddd'],
+							[ 'aaa', 'bbb', 'ccc', 'dddd'],
+							[ 'aaa', 'bbb', 'ccc', 'dddd'],
+							[ 'aaa', 'bbb', 'ccc', 'dddd'],
+							[ 'aaa', 'bbb', 'ccc', 'dddd'],
+							[ 'aaa', 'bbb', 'ccc', 'dddd'],
+							[ 'aaa', 'bbb', 'ccc', 'dddd'],
+							[ 'aaa', 'bbb', 'ccc', 'dddd'],
+							[ 'aaa', 'bbb', 'ccc', 'dddd'],
+							[ 'aaa', 'bbb', 'ccc', 'dddd'],
+							[ 'aaa', 'bbb', 'ccc', 'dddd'],
+							[ 'aaa', 'bbb', 'ccc', 'dddd'],
+							[ 'aaa', 'bbb', 'ccc', 'dddd'],
+							[ 'aaa', 'bbb', 'ccc', 'dddd'],
+							[ 'aaa', 'bbb', 'ccc', 'dddd'],
+							[ 'aaa', 'bbb', 'ccc', 'dddd'],
+							[ 'aaa', 'bbb', 'ccc', 'dddd'],
+							[ 'aaa', 'bbb', 'ccc', 'dddd'],
+							[ 'aaa', 'bbb', 'ccc', 'dddd'],
+							[ 'aaa', 'bbb', 'ccc', 'dddd'],
+							[ 'aaa', 'bbb', 'ccc', 'dddd'],
+							[ 'aaa', 'bbb', 'ccc', 'dddd'],
+							[ 'aaa', 'bbb', 'ccc', 'dddd'],
+							[ 'aaa', 'bbb', 'ccc', 'dddd'],
+							[ 'aaa', 'bbb', 'ccc', 'dddd'],
+							[ 'aaa', 'bbb', 'ccc', 'dddd'],							
+							[ 'aaa', 'bbb', 'ccc', 'dddd']
+						]
+					}									
+				}					    
 			],
 			defaultStyle: {
-			font: 'THSarabun'
+				font: 'THSarabun'
 			},
 			images : {
 				nu_logo : base64_nu_logo,
 				nhso_logo : base64_nhso_logo
 			}
  		};
- 		pdfMake.fonts = font;
 
- 		
-
+ 		pdfMake.fonts = font; 		
+ 		// pdfMake.createPdf(docDefinition).open();
  		pdfMake.createPdf(docDefinition).getBase64(function (base64) {
 			$('iframe').attr('src', "data:application/pdf;base64," + base64);
 		});
